a717c72b048859f5ac245453604e18a244cd57a6
Egon Fiedler <efiedler93@gmail.com>
2018-09-03 12:20:46 -0700

    updated the root route. mongo setup but missing database connection

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ const express = require('express');
 const app = express();
 //Setup the template engine
 var exphbs = require('express-handlebars');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let reviews = [
-    { title: "Great Review" },
-    { title: "Next Review" } 
-];
+//////////////////////////////////////////////
+// This will connect with the Database MONGODB
+//////////////////////////////////////////////
+//Setsup the Object Document Manager
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+const Review = mongoose.model('Review', {
+    title: String
+});
 
 //////////////////////////////////////////////
 // These are the routes
 //////////////////////////////////////////////
 //Reviews route, here you are requesting from the backend and responding to it
 //the response renders through handlebars all the reviews
-app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
-}); //Javascript commetaries: you can nest calls to call other things that do 
-    //another step of the process
+app.get( '/', (req , res) => {
+    Review.find() //Return a premise, it is a value that will provided 
+        .then(reviews => {
+            res.render('reviews-index', { reviews: reviews})
+        })
+        .catch(err => {
+            console.log(err)
+        });
+});
 
 //////////////////////////////////////////////
 // This will connect with the web
@@ -33,3 +42,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
+
