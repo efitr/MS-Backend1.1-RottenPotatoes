d7e33f9976e584985b83b33472a8d59e7f0c7a5a
Egon Fiedler <efiedler93@gmail.com>
2018-09-03 10:55:57 -0700

    root route updated, handlebars added reviews-index

--- app.js
+++ app.js
@@ -3,27 +3,29 @@
 // Basic set up
 //////////////////////////////////////////////
 //Create the entry to the setup of the framework
-const express = require('express')
+const express = require('express');
 //Setup the framework
-const app = express()
+const app = express();
 //Setup the template engine
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" } 
+];
 
 //////////////////////////////////////////////
 // These are the routes
 //////////////////////////////////////////////
-//Basic route that gets simple text
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-//Root route that renders the String through Handlebars home file
+//Reviews route, here you are requesting from the backend and responding to it
+//the response renders through handlebars all the reviews
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!' });
-});
+    res.render('reviews-index', { reviews: reviews });
+}); //Javascript commetaries: you can nest calls to call other things that do 
+    //another step of the process
 
 //////////////////////////////////////////////
 // This will connect with the web
