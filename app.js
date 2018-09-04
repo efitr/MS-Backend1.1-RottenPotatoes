d0865def5cf0fc7c1959281ed332989a46cb0181
Egon Fiedler <efiedler93@gmail.com>
2018-09-04 16:17:15 -0700

    root broken, new reviews eoute added

--- app.js
+++ app.js
@@ -16,11 +16,17 @@ app.set('view engine', 'handlebars');
 //////////////////////////////////////////////
 //Setsup the Object Document Manager
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 const Review = mongoose.model('Review', {
     title: String
 });
 
+//Hardcoded resources
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+]
+  
 //////////////////////////////////////////////
 // These are the routes
 //////////////////////////////////////////////
@@ -32,8 +38,12 @@ app.get( '/', (req , res) => {
             res.render('reviews-index', { reviews: reviews})
         })
         .catch(err => {
-            console.log(err)
+            console.error(err)
         });
+});
+//New Reviews route
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
 });
 
 //////////////////////////////////////////////
