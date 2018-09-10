9cbf4d5590a71297cc463764b4593af19a663e5a
Egon Fiedler <efiedler93@gmail.com>
2018-09-10 11:20:40 -0700

    improved new and edit templates with DRY code adding reviews-form template

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 var http = require('http');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 //////////////////////////////////////////////
 // Database MONGODB
@@ -28,12 +31,6 @@ const Review = mongoose.model('Review', {
     movieTitle: String
 });
 
-//Hardcoded resources
-// let reviews = [
-//     { title: "Great Review" },
-//     { title: "Next Review" }
-// ];
-  
 //////////////////////////////////////////////
 // ROUTES
 //////////////////////////////////////////////
@@ -60,7 +57,7 @@ app.post('/reviews', (req, res) => {
   })
   .catch((err) => {
     console.log(err.message)
-  })
+  });
 });
 //GET specific rev
 app.get('/reviews/:id', (req, res) => {
@@ -77,6 +74,16 @@ app.get('/reviews/:id/edit', function (req, res) {
   Review.findById(req.params.id, function(err, review) {
     res.render('reviews-edit', {review: review});
   })
+});
+//PUT
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
 });
 
 //////////////////////////////////////////////
