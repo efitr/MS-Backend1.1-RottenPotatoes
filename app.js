cde100b28ecd39b4e5205012e61d6cfa3b3567b2
Egon Fiedler <efiedler93@gmail.com>
2018-09-09 22:40:54 -0700

    New template for reviews, added one line to the index template, added route to search with id:

--- app.js
+++ app.js
@@ -4,21 +4,24 @@
 //////////////////////////////////////////////
 //Create the entry to the setup of the framework
 const express = require('express');
-//Setup the framework
 const app = express();
-//Setup the template engine
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+var http = require('http');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-//Initialize the body parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+
 //////////////////////////////////////////////
-// This will connect with the Database MONGODB
+// Database MONGODB
 //////////////////////////////////////////////
-//Setsup the Object Document Manager
+//Setup Object Document Manager
+//Create the mongoose conection 
 const mongoose = require('mongoose');
+//Maybe I should handle this better
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
 const Review = mongoose.model('Review', {
     title: String,
     description: String,
@@ -32,31 +35,42 @@ const Review = mongoose.model('Review', {
 // ];
   
 //////////////////////////////////////////////
-// These are the routes
+// ROUTES
 //////////////////////////////////////////////
-//Reviews route, here you are requesting from the backend and responding to it
-//the response renders through handlebars all the reviews
+//GET root
 app.get( '/', (req , res) => {
-    Review.find() //Return a premise, it is a value that will provided 
-        .then(reviews => {
-            res.render('reviews-index', { reviews: reviews})
-        })
-        .catch(err => {
-            console.log(err.message)
-        })
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews })
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 });
-//New Reviews route
+//GET review
 app.get('/reviews/new', (req, res) => {
     res.render('reviews-new', {});
 });
-//
+//POST review
 app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect('/');
-    }).catch((err) => {
-        console.log(err.message);
+  Review.create(req.body)
+    .then((review) => {
+      console.log(review);
+      res.redirect('/');
     })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+//GET specific rev
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id)
+  .then((review) => {
+    res.render('reviews-show', { review: review })
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 });
 
 //////////////////////////////////////////////
