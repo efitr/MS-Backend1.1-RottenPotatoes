36b4d2e47c3342953fe6ead729abc5940c4017d2
Egon Fiedler <efiedler93@gmail.com>
2018-09-19 01:11:06 -0700

    styling

--- app.js
+++ app.js
@@ -2,54 +2,33 @@
 //////////////////////////////////////////////
 // Basic set up
 //////////////////////////////////////////////
+//This will be importing the neccesary controllers
+const reviews = require('./controllers/reviews')
+const comments = require('./controllers/comments')
+
 //Create the entry to the setup of the framework
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-var http = require('http');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json()); //quite likely unnecesary
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'));
 
-// //////////////////////////////////////////////
-// // Database MONGODB
-// //////////////////////////////////////////////
-// //Setup Object Document Manager
-// //Create the mongoose conection 
-// var mongoose = require('mongoose');
-// //Maybe I should handle this better
-// ////////////
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/rotten-potatoes', {useNewUrlParser: true});
-
-// mongoose.connect(
-//   .then(() => {
-//     console.log("MongoDB is functioning");
-//   })
-//   .catch( err => {
-//     throw err;
-//   })
-
-// var reviewSchema = new mongoose.Schema({
-//     title: String,
-//     description: String,
-//     movieTitle: String
-// });
-
-// var Review = mongoose.model("Review", reviewSchema);
-
 //////////////////////////////////////////////
 // Resources for networking in .controller/reviews.js
 //////////////////////////////////////////////
+// const reviewRoute = require("./controllers/reviews.js")(app)
 
-//I'm supposed to get the routes from reviews.js
-const reviewRoute = require("./controllers/reviews.js")(app)
+reviews(app);
+comments(app);
+
+module.exports = app;
 
 //////////////////////////////////////////////
 // This will connect with the web
